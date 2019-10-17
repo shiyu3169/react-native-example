d221b65006261d4823e79498df5f64d9a9bd34ab
Shiyu Wang <shiyu3169@gmail.com>
2019-10-17 11:41:00 -0400

    inline style

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [outputText, setOutputText] = useState(
@@ -7,21 +7,28 @@ export default function App() {
   );
 
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Change Text"
-        onPress={() => setOutputText("The text changed!")}
-      />
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+            width: "80%"
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const styles = StyleSheet.create({});
