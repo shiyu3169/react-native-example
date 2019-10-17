ba02a78af26bf7226f14c5f3f68733e4ce9e8bf1
Shiyu Wang <shiyu3169@gmail.com>
2019-10-17 14:40:55 -0400

    styles

--- App.js
+++ App.js
@@ -7,23 +7,9 @@ export default function App() {
   );
 
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center"
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-            width: "80%"
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.inputContainer} />
         <Button title="ADD" />
       </View>
       <View></View>
@@ -31,4 +17,19 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  }
+});
